34b591ed4c04209222c3de6bcfa59a73d78f7a38
munchkinhalfling <munchkinhalfling@pm.me>
2020-12-21 17:20:54 +0000

    Remove unnecessary import

--- app.tsx
+++ app.tsx
@@ -1,6 +1,5 @@
 import { render } from 'preact';
 import App from './components/App';
-import Term from './util/Term';
 import terms from './terms.json';
 
 render((<App terms={terms.terms}></App>), document.querySelector("#app"));
