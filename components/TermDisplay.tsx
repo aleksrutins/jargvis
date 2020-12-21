05e5559ba822256aaf01fc91681f84390bfc7abe
munchkinhalfling <munchkinhalfling@pm.me>
2020-12-21 19:00:50 +0000

    Add alt text

--- components/TermDisplay.tsx
+++ components/TermDisplay.tsx
@@ -6,7 +6,7 @@ export default class TermDisplay extends Component<{term: Term}> {
         return (<div>
             <h2>{term.term}</h2>
             <p>{term.definition}</p>
-            <img src={imgUrl(term.img)} width="400" height="300"></img>
+            <img src={imgUrl(term.img)} width="400" height="300" alt={term.term} title={term.term}></img>
         </div>
         )
     }
